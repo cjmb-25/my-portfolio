cfb4c9cbd37ef2f52232e708b64d1179416437fe
cjmb-25 <mcjbarrios@tip.edu.ph>
2025-06-20 12:49:35 +0800

    some edits

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -39,19 +39,19 @@ export const Skills = () => {
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Network Design</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Security Operations Center</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>I.T. infrastracture Operations</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Cybersecurity Compliance</h5>
                             </div>
                         </Carousel>
                     </div>
