f10e15c7c069c923d54f894f2dee2826d57033c8
cjmb-25 <mcjbarrios@tip.edu.ph>
2025-06-28 10:16:34 +0800

    removed let's connect

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -54,9 +54,6 @@ export const NavBar = () => {
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
